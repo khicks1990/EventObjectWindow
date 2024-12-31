0c86333e760882240c3abc0e7be9d1848e961385
khicks1990 <85317318+khicks1990@users.noreply.github.com>
2024-12-31 06:59:57 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.border.Border;
+
+public class Main {
+
+   /**
+      main method
+   */
+
+   public static void main(String[] args)
+   {
+      new EventObject2();
+   }
+}
